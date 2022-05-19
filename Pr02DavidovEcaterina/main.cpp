7125ff4c0ea1064deca5316b8a8452133b1d126d
DavidovEcaterina <90680113+DavidovEcaterina@users.noreply.github.com>
2022-05-20 01:44:56 +0300

    Add files via upload

---
+++ Pr02DavidovEcaterina/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+1.Calculam numarul de cifre
+2.Verificam proprietatea
+*/
+
+int NrCifre(long a)
+{
+    int kol = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        kol++;
+    }
+    return kol;
+}
+
+long power(long a, long b)
+{
+    long prod = 1;
+    for (long i = 1; i <= b; i++)
+    {
+        prod *= a;
+    }
+    return prod;
+}
+
+bool Armstrong(long nr)
+{
+    long sum = 0, cf, col = NrCifre(nr);
+    long number = nr;
+    while (number > 0)
+    {
+        cf = number % 10;
+        sum += power(cf, col);
+        number /= 10;
+    }
+    return (sum == nr);
+}
+void NumereArmstrongInterval(int a, int b)
+{
+for(int i = a; i <= b; i++)
+    {
+        if(Armstrong(i))
+            {
+                cout << i << " ";
+            }
+    }
+}
+void AfiseazaNumereArmstrong(int n)
+{
+    long i = 1;
+    while(n > 0)
+        {
+             if(Armstrong(i))
+            {
+                cout << i << " " ;
+                n--;
+            }
+            i++;
+        }
+}
+bool Prim(int n)
+{
+    if(n <= 1)return false;
+    for(int i = 2; i <= n / 2; i++)
+        {
+            if(n % i == 0 ) return false;
+        }
+    return true;
+}
+void NumereArmstrongPrimeInterval(int a, int b)
+{
+for(int i = a; i <= b; i++)
+    {
+        if(Armstrong(i)&&Prim(i))
+            {
+                cout << i << " ";
+            }
+    }
+}
+int main()
+{
+    cout << "Proiect nr.2 elaborat de Davidov Ecaterina" << endl;
+    cout << "Tema: Numere Armstrong" << endl;
+    long nr;
+    cout << "Introduceti un numar: ";
+    cin >> nr;
+    Armstrong(nr) ? cout << nr << " este numar Armstrong" << endl : cout << nr << " nu este numar Armstrong" << endl;
+    int x, y;
+    cout << "Introduceti limita inferioara: ";
+    cin >> x;
+    cout << "Introduceti limita superiora: ";
+    cin >> y;
+    cout << "Numerele Armstrong din intervalul [" << x << ";" << y << "]: " <<endl;
+    NumereArmstrongInterval(x, y);
+    int num;
+    cout << "\nIntroduceti numarul de numere Armstrong dorit: ";
+    cin >> num;
+    cout << "Primele " << num << " numere Armstrong: ";
+    AfiseazaNumereArmstrong(num);
+    cout << "\nNumerele ArmstrongPrime din intervalul [" << x << ";" << y << "]: " <<endl;
+    NumereArmstrongPrimeInterval(x, y);
+    return 0;
+}
